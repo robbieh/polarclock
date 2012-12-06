01abe8c35b5fc6c0cb1471699c775139e9fda39e
Robbie Huffman <robbie.huffman@gmail.com>
2012-12-06 13:19:27 -0500

    Added a 'GMT dot' and hour marking arc

--- src/polarclock/core.clj
+++ src/polarclock/core.clj
@@ -1,7 +1,9 @@
 (ns polarclock.core
   (:gen-class)
   (:use quil.core)
-  (:require [clj-time.core :as clj-time]))
+  (:require [clj-time.core :as clj-time]
+            [clj-time.local]
+            ))
 
 
 (defn setup []
@@ -10,12 +12,22 @@
   (frame-rate 1)
   (background 0))
 
+(defn hour-to-radians [hr]
+  ; there are 15 degrees "per hour" on a 24-hr clock
+  (radians (* 15 hr)))
+
+(defn minutes-to-radians [minutes]
+  (* TWO-PI (radians minutes)))
+
 (defn draw-radclock []
   (background 0)
   (let [diam  (* 0.9 (min (width) (height)))
+        tmdiam (* 0.8 (min (width) (height)))
         x     (* (width) 0.5)
         y     (* (height) 0.5)
-        stoprad  (* TWO-PI (radians (clj-time/minute (clj-time/now))))
+        stoprad  (minutes-to-radians (clj-time/minute (clj-time/now)))
+        gmtrad  (hour-to-radians (clj-time/hour (clj-time/now)))
+        hourrad  (hour-to-radians (clj-time/hour (clj-time.local/local-now)))
         ]
     (translate x y)
     (rotate (- HALF-PI))
@@ -25,13 +37,22 @@
     (stroke 0 100 0)
     (stroke-weight 16)
     (arc 0 0 diam diam 0 stoprad)
+    ; let's see... red 'dot' on the GMT hour
+    (stroke 200 50 50)
+    (stroke-weight 5)
+    (arc 0 0 tmdiam tmdiam (- gmtrad (radians 1)) (+ gmtrad (radians 1)))
+    ; an hour-size mark for the hour
+    (stroke 0 255 0)
+    (stroke-weight 1)
+    (arc 0 0 tmdiam tmdiam hourrad (+ hourrad (radians 15)))
+    
   ))
 
 (defsketch example
            :title "polar clock"
            :setup setup
            :draw draw-radclock
-           :size [323 200])
+           :size [500 500])
 
 (defn -main
   "I don't do a whole lot ... yet."
