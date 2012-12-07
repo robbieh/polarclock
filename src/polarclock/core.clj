b0fd6a6f41cf5b8df26130520ecbce920d234d32
Robbie Huffman <robbie.huffman@gmail.com>
2012-12-06 21:00:51 -0500

    just playing around

--- src/polarclock/core.clj
+++ src/polarclock/core.clj
@@ -2,32 +2,40 @@
   (:gen-class)
   (:use quil.core)
   (:require [clj-time.core :as clj-time]
-            [clj-time.local]
-            ))
+            [clj-time.local])
+  (:import (java.util Calendar Date))
+  )
 
 
 (defn setup []
   (smooth)
   (no-fill)
   (frame-rate 1)
-  (background 0))
+  (background 0 50 10))
 
 (defn hour-to-radians [hr]
   ; there are 15 degrees "per hour" on a 24-hr clock
   (radians (* 15 hr)))
 
+(defn hour-to-radians-12 [hr]
+  (* 2 (hour-to-radians (if (> 12 hr) hr (- hr 12)))))
+
 (defn minutes-to-radians [minutes]
-  (* TWO-PI (radians minutes)))
+  (radians minutes))
+
+;(defn radians-for-this-month []
+;  (
 
 (defn draw-radclock []
-  (background 0)
+  (background 180 200 150)
   (let [diam  (* 0.9 (min (width) (height)))
         tmdiam (* 0.8 (min (width) (height)))
         x     (* (width) 0.5)
         y     (* (height) 0.5)
         stoprad  (minutes-to-radians (clj-time/minute (clj-time/now)))
         gmtrad  (hour-to-radians (clj-time/hour (clj-time/now)))
-        hourrad  (hour-to-radians (clj-time/hour (clj-time.local/local-now)))
+        hourrad  (hour-to-radians-12 (clj-time/hour (clj-time.local/local-now)))
+        onerad (radians 1)
         ]
     (translate x y)
     (rotate (- HALF-PI))
@@ -40,12 +48,17 @@
     ; let's see... red 'dot' on the GMT hour
     (stroke 200 50 50)
     (stroke-weight 5)
-    (arc 0 0 tmdiam tmdiam (- gmtrad (radians 1)) (+ gmtrad (radians 1)))
+    (arc 0 0 tmdiam tmdiam (- gmtrad onerad) (+ gmtrad onerad))
     ; an hour-size mark for the hour
     (stroke 0 255 0)
     (stroke-weight 1)
-    (arc 0 0 tmdiam tmdiam hourrad (+ hourrad (radians 15)))
-    
+    (arc 0 0 tmdiam tmdiam hourrad (+ hourrad (radians 30))) ; 30 because 12h clock!
+    (stroke-weight 1)
+;   ;this shows 'hour markers' for a 24h clock
+;    (doseq [x (range 0 23)]
+;      (let [h (hour-to-radians x)
+;            rdiff (radians 1)]
+;            (arc 0 0 tmdiam tmdiam (- h rdiff) (+ h rdiff))))
   ))
 
 (defsketch example
