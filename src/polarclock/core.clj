a95abbfcbde5abd81875576c50f00b645dc555bc
Robbie Huffman <robbie.huffman@gmail.com>
2012-12-07 15:57:15 -0500

    added arc to day, line to connect hours to minutes

--- src/polarclock/core.clj
+++ src/polarclock/core.clj
@@ -51,55 +51,50 @@
   (rotate (- PI))
   (push-matrix)
   (push-style)
-  (stroke 0 75 55)
+  (stroke 0 75 25)
   (stroke-weight 1)
   (let [x     (* (width) 0.5)
         y     (* (height) 0.5)]
     (doseq [day (range 0 30)]
-      (line 0 (* (height) (* 0.8 0.5))  0 (* (height) (* 0.9 0.5)))
+      (line 0 (* (height) (* 0.90 0.5))  0 (* (height) (* 0.95 0.5)))
       (rotate (radians-for-30d))
       )
     )
   (stroke-weight 3)
-  (stroke 0 95 75)
+  (stroke 10 95 0)
   (pop-matrix)(push-matrix)
-  (rotate (* (clj-time/day (clj-time/now)) (radians-for-30d)))
-    (line 0 (* (height) (* 0.75 0.5))  0 (* (height) (* 0.9 0.5)))
+  (let [diam (* 0.95 (min (width) (height)))]
+    (rotate (* (clj-time/day (clj-time/now)) (radians-for-30d)))
+    (line 0 (* (height) (* 0.85 0.5))  0 (* (height) (* 0.95 0.5)))
+    (rotate (- (radians-for-30d)))
+    (line 0 (* (height) (* 0.85 0.5))  0 (* (height) (* 0.95 0.5)))
+    (stroke-weight 5)
+    (rotate HALF-PI)
+    (arc 0 0 diam diam 0 (radians-for-30d))
+    )
   (pop-style)
   (pop-matrix))
 
 (defn draw-radclock []
   (push-matrix)
   (push-style)
-  (let [diam  (* 0.6 (min (width) (height)))
-        tmdiam (* 0.5 (min (width) (height)))
+  (let [diam  (* 0.8 (min (width) (height)))
+        tmdiam (* 0.7 (min (width) (height)))
         x     (* (width) 0.5)
         y     (* (height) 0.5)
         stoprad  (minutes-to-radians (clj-time/minute (clj-time/now)))
         gmtrad  (hour-to-radians (clj-time/hour (clj-time/now)))
         hourrad  (hour-to-radians-12 (clj-time/hour (clj-time.local/local-now)))
         onerad (radians 1)
+        minofhourrad (/ stoprad 14)
         ]
     (rotate (- HALF-PI))
 
-    ; the minutes ; draw two-part outline
-    (stroke 0 90 0)
-    (stroke-weight 20)
-    (arc 0 0 diam diam 0 stoprad)
-    (stroke 0 220 20)
-    (stroke-weight 12)
-    (arc 0 0 diam diam 0 stoprad)
-
-    ; let's see... red 'dot' on the GMT hour
-    (stroke 250 25 25)
-    (stroke-weight 5)
-    (arc 0 0 tmdiam tmdiam (- gmtrad onerad) (+ gmtrad onerad))
-
     ; an hour-size mark for the hour
-    (stroke 50 200 95)
-    (stroke-weight 1)
+    ;(stroke 50 200 95)
+    (stroke 0 220 20)
+    (stroke-weight 3)
     (arc 0 0 tmdiam tmdiam hourrad (+ hourrad (radians 30))) ; 30 because 12h clock!
-    (stroke-weight 1)
     ;   ;this shows 'hour markers' for a 24h clock
     ;    (doseq [x (range 0 23)]
     ;      (let [h (hour-to-radians x)
@@ -107,11 +102,32 @@
     ;            (arc 0 0 tmdiam tmdiam (- h rdiff) (+ h rdiff))))
 
     ; and an extra minute-dot on the hour...
-    (stroke-weight 4)
-    (let [minofhourrad (/ stoprad 14)
-          ]
-      (arc 0 0 tmdiam tmdiam (+ hourrad minofhourrad -0.01) (+ hourrad minofhourrad 0.01)))
+    ; with a line out to the minutes ring
+    (stroke-weight 5)
+    (arc 0 0 tmdiam tmdiam (+ hourrad minofhourrad -0.01) (+ hourrad minofhourrad 0.01))
 
+    ; the minutes ; draw two-part outline
+    (stroke 0 220 20)
+    (stroke-weight 3)
+    (arc 0 0 diam diam 0 minofhourrad)
+    (let []
+      (push-matrix)
+      (rotate (+ 0 minofhourrad))
+      (line 0 (* tmdiam 0.5)  0 (* diam 0.5))
+      (pop-matrix)
+      )
+
+    (stroke 0 90 0)
+    (stroke-weight 20)
+    (arc 0 0 diam diam 0 stoprad)
+    (stroke 0 220 20)
+    (stroke-weight 16)
+    (arc 0 0 diam diam 0 stoprad)
+
+    ; let's see... red 'dot' on the GMT hour
+    (stroke 250 25 25)
+    (stroke-weight 5)
+    (arc 0 0 tmdiam tmdiam (- gmtrad onerad) (+ gmtrad onerad))
     (pop-style)
     (pop-matrix)
     ))
@@ -136,4 +152,5 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (mksk))
+
